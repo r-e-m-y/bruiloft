c983f72e46572011c3a6a4ad5d83c28cfc131d1f
Remy Brokke <remy@nlvoorelkaar.nl>
2025-01-17 19:48:46 +0100

    First commit

---
+++ src/database/migrations/2025_01_17_185512_create_visitors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone')->nullable();
+            $table->text('allergy_diet')->nullable();
+            $table->string('main_course');
+            $table->string('dessert');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visitors');
+    }
+};
